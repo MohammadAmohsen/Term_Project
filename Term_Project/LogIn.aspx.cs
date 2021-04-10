2254296730e57336308918814f201ee61d5017d2
Mohammad Abdelmohsen <moeabd99@outlook.com>
2021-04-09 21:23:05 -0400

    finished cookie and encryption

--- Term_Project/LogIn.aspx.cs
+++ Term_Project/LogIn.aspx.cs
@@ -64,64 +64,25 @@ namespace Term_Project
                 if (size > 0)
                 {
                     string Type = db.GetField("Type", 0).ToString();
-
+ String user = txtEmail.Text;
+                        String plainTextPassword = txtPassword.Text;
                     if (cbRememberMe.Checked)
                     {
-                        String user = txtEmail.Text;
-                        String plainTextPassword = txtPassword.Text;
-                        String encryptedPassword;
+                       
 
-
-                        UTF8Encoding encoder = new UTF8Encoding();      // used to convert bytes to characters, and back
-                        Byte[] textBytes;                               // stores the plain text data as bytes
-
-
-
-                        // Perform Encryption
-                        //-------------------
-                        // Convert a string to a byte array, which will be used in the encryption process.
-                        textBytes = encoder.GetBytes(plainTextPassword);
-
-
-                        // Create an instances of the encryption algorithm (Rinjdael AES) for the encryption to perform,
-                        // a memory stream used to store the encrypted data temporarily, and
-                        // a crypto stream that performs the encryption algorithm.
-                        RijndaelManaged rmEncryption = new RijndaelManaged();
-                        MemoryStream myMemoryStream = new MemoryStream();
-                        CryptoStream myEncryptionStream = new CryptoStream(myMemoryStream, rmEncryption.CreateEncryptor(key, vector), CryptoStreamMode.Write);
-
-
-                        // Use the crypto stream to perform the encryption on the plain text byte array.
-                        myEncryptionStream.Write(textBytes, 0, textBytes.Length);
-                        myEncryptionStream.FlushFinalBlock();
-
-
-                        // Retrieve the encrypted data from the memory stream, and write it to a separate byte array.
-                        myMemoryStream.Position = 0;
-                        Byte[] encryptedBytes = new Byte[myMemoryStream.Length];
-                        myMemoryStream.Read(encryptedBytes, 0, encryptedBytes.Length);
-
-
-                        // Close all the streams.
-                        myEncryptionStream.Close();
-                        myMemoryStream.Close();
-
-                        // Convert the bytes to a string and display it.
-                        encryptedPassword = Convert.ToBase64String(encryptedBytes);
-
-
-                        // Write encrypted password to a cookie
-                        HttpCookie myCookie = new HttpCookie("LoginCookie");
-                        myCookie.Values["Username"] = user;
-                        myCookie.Values["Password"] = encryptedPassword;
-                        myCookie.Expires = new DateTime(2025, 2, 1);
-                        Response.Cookies.Add(myCookie);
+                        CreateCookie(plainTextPassword, user);
 
                         logIN(Type);
 
                     }
                     else
                     {
+                        EncryptPassword(plainTextPassword, user);
+                        HttpCookie myCookie = Request.Cookies["LoginCookie"];
+
+                         Response.Write("<script>alert('Not Checked CookieUser = " + myCookie.Values["Username"] + "')</script>");
+                        Response.Write("<script>alert('Not Checked Cookie Password = " + myCookie.Values["Password"] + "')</script>");
+ 
                         logIN(Type);
 
                     }
@@ -143,7 +104,7 @@ namespace Term_Project
 
         private void logIN(string Type)
         {
-            if (Type.CompareTo("User") == 0)
+                 if (Type.CompareTo("User") == 0)
             {
                 Session["UserId"] = db.GetField("UserId", 0);
                 Session["FirstName"] = db.GetField("FirstName", 0);
@@ -184,8 +145,147 @@ namespace Term_Project
                 Session["UserGoals"] = db.GetField("UserGoals", 0);
                 Response.Redirect("AdminPage.aspx");
             }
+     }
+
+        private void CreateCookie(string plainTextPassword, string user)
+        {
+            String encryptedPassword;
+
+            UTF8Encoding encoder = new UTF8Encoding();      // used to convert bytes to characters, and back
+            Byte[] textBytes;                               // stores the plain text data as bytes
+
+
+
+            // Perform Encryption
+            //-------------------
+            // Convert a string to a byte array, which will be used in the encryption process.
+            textBytes = encoder.GetBytes(plainTextPassword);
+
+
+            // Create an instances of the encryption algorithm (Rinjdael AES) for the encryption to perform,
+            // a memory stream used to store the encrypted data temporarily, and
+            // a crypto stream that performs the encryption algorithm.
+            RijndaelManaged rmEncryption = new RijndaelManaged();
+            MemoryStream myMemoryStream = new MemoryStream();
+            CryptoStream myEncryptionStream = new CryptoStream(myMemoryStream, rmEncryption.CreateEncryptor(key, vector), CryptoStreamMode.Write);
+
+
+            // Use the crypto stream to perform the encryption on the plain text byte array.
+            myEncryptionStream.Write(textBytes, 0, textBytes.Length);
+            myEncryptionStream.FlushFinalBlock();
+
+
+            // Retrieve the encrypted data from the memory stream, and write it to a separate byte array.
+            myMemoryStream.Position = 0;
+            Byte[] encryptedBytes = new Byte[myMemoryStream.Length];
+            myMemoryStream.Read(encryptedBytes, 0, encryptedBytes.Length);
+
+
+            // Close all the streams.
+            myEncryptionStream.Close();
+            myMemoryStream.Close();
+
+            // Convert the bytes to a string and display it.
+            encryptedPassword = Convert.ToBase64String(encryptedBytes);
+
+
+            // Write encrypted password to a cookie
+            HttpCookie myCookie = new HttpCookie("LoginCookie");
+            myCookie.Values["Username"] = user;
+            myCookie.Values["Password"] = plainTextPassword;
+            myCookie.Expires = new DateTime(2025, 2, 1);
+            Response.Cookies.Add(myCookie);
         }
 
-      
+        private void EncryptPassword(string plainTextPassword, string user)
+        {
+            String encryptedPassword;
+
+            UTF8Encoding encoder = new UTF8Encoding();      // used to convert bytes to characters, and back
+            Byte[] textBytes;                               // stores the plain text data as bytes
+
+
+
+            // Perform Encryption
+            //-------------------
+            // Convert a string to a byte array, which will be used in the encryption process.
+            textBytes = encoder.GetBytes(plainTextPassword);
+
+
+            // Create an instances of the encryption algorithm (Rinjdael AES) for the encryption to perform,
+            // a memory stream used to store the encrypted data temporarily, and
+            // a crypto stream that performs the encryption algorithm.
+            RijndaelManaged rmEncryption = new RijndaelManaged();
+            MemoryStream myMemoryStream = new MemoryStream();
+            CryptoStream myEncryptionStream = new CryptoStream(myMemoryStream, rmEncryption.CreateEncryptor(key, vector), CryptoStreamMode.Write);
+
+
+            // Use the crypto stream to perform the encryption on the plain text byte array.
+            myEncryptionStream.Write(textBytes, 0, textBytes.Length);
+            myEncryptionStream.FlushFinalBlock();
+
+
+            // Retrieve the encrypted data from the memory stream, and write it to a separate byte array.
+            myMemoryStream.Position = 0;
+            Byte[] encryptedBytes = new Byte[myMemoryStream.Length];
+            myMemoryStream.Read(encryptedBytes, 0, encryptedBytes.Length);
+
+
+            // Close all the streams.
+            myEncryptionStream.Close();
+            myMemoryStream.Close();
+
+            // Convert the bytes to a string and display it.
+            encryptedPassword = Convert.ToBase64String(encryptedBytes);
+
+
+            // Write encrypted password to a cookie
+            HttpCookie myCookie = new HttpCookie("LoginCookie");
+            myCookie.Values["Username"] = user;
+            myCookie.Values["Password"] = encryptedPassword;
+            myCookie.Expires = new DateTime(2025, 2, 1);
+            Response.Cookies.Add(myCookie);
+        }
+
+        private void UseCookie()
+        {
+            HttpCookie myCookie = Request.Cookies["LoginCookie"];
+            String encryptedPassword = myCookie.Values["Password"];
+            Byte[] encryptedPasswordBytes = Convert.FromBase64String(encryptedPassword);
+            Byte[] textBytes;
+            String plainTextPassword;
+            UTF8Encoding encoder = new UTF8Encoding();
+
+
+            // Perform Decryption
+            //--------------------
+            // Create an instances of the decryption algorithm (Rinjdael AES) for the encryption to perform,
+            // a memory stream used to store the decrypted data temporarily, and
+            // a crypto stream that performs the decryption algorithm.
+            RijndaelManaged rmEncryption = new RijndaelManaged();
+            MemoryStream myMemoryStream = new MemoryStream();
+            CryptoStream myDecryptionStream = new CryptoStream(myMemoryStream, rmEncryption.CreateDecryptor(key, vector), CryptoStreamMode.Write);
+
+
+            // Use the crypto stream to perform the decryption on the encrypted data in the byte array.
+            myDecryptionStream.Write(encryptedPasswordBytes, 0, encryptedPasswordBytes.Length);
+            myDecryptionStream.FlushFinalBlock();
+
+
+            // Retrieve the decrypted data from the memory stream, and write it to a separate byte array.
+            myMemoryStream.Position = 0;
+            textBytes = new Byte[myMemoryStream.Length];
+            myMemoryStream.Read(textBytes, 0, textBytes.Length);
+
+
+            // Close all the streams.
+            myDecryptionStream.Close();
+            myMemoryStream.Close();
+
+            // Convert the bytes to a string and display it.
+            plainTextPassword = encoder.GetString(textBytes);
+
+        }
+
     }
 }
